7ff97f2f94cc67fb878dc78c22e8a0e3a149b526
kappa <kappa@DESKTOP-TOMAS.lan>
2021-12-27 23:10:19 +0000

    feat: added clipboard support

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,15 +1,26 @@
+import { useState } from 'react';
+
 import {
   Avatar,
+  Box,
+  Button,
   Container,
   Flex,
   Heading,
   Spacer,
   Text,
+  useClipboard,
 } from '@chakra-ui/react';
 
-import {BsTwitter } from 'react-icons'
+import { BsTwitter, BsClipboard } from 'react-icons/bs';
+import { AiFillEyeInvisible } from 'react-icons/ai';
 
 const Index = () => {
+  const [isVisible, setIsVisible] = useState(false);
+  const [clipboard, setClipboard] = useState('password');
+
+  const { hasCopied, onCopy } = useClipboard(clipboard);
+
   return (
     <>
       <Flex m={50} alignItems='center' justifyContent='space-between'>
@@ -20,17 +31,36 @@ const Index = () => {
         <Spacer />
         <Avatar />
       </Flex>
+
       <Text as='h2' ml={110} fontSize='2xl'>
         <b>Hello,</b> Example
       </Text>
-      <Container mt={16}>
-        <Flex alignItems='center' justifyContent='space-around'>
+
+      <Container mt={16} maxW='container.md'>
+        <Flex alignItems='center' justifyContent='space-between'>
           <Text fontSize='xl'>Service</Text>
           <Text fontSize='xl'>Password</Text>
         </Flex>
 
-        <Flex alignItems='center' justifyContent='space-around'>
-          <
+        <Flex alignItems='center' justifyContent='space-between  ' mt={4}>
+          <BsTwitter size={30} />
+          <Flex alignItems='center' justifyContent='center'>
+            <Box
+              mr={4}
+              cursor='pointer'
+              onClick={() => setIsVisible(state => !state)}
+            >
+              {isVisible ? (
+                <Text>Password</Text>
+              ) : (
+                <AiFillEyeInvisible size={30} />
+              )}
+            </Box>
+            <Button onClick={onCopy} ml={2}>
+              <BsClipboard cursor='copy' size={22} />
+              <Text>{hasCopied ? 'Copied' : 'Copy'}</Text>
+            </Button>
+          </Flex>
         </Flex>
       </Container>
     </>
